df160016ee80795b3d3d7f7462bb252babcdf6ed
Francois Leurent <131.code@leurent.email>
2022-10-20 14:02:38 +0200

    Fix linter

--- test/index.js
+++ test/index.js
@@ -5,17 +5,17 @@ const path = require('path');
 
 const expect = require('expect.js');
 
-const dspp   = require('../');
+const Dspp   = require('../');
 
 
-describe("Initial dspp check", function(){
+describe("Initial dspp check", function() {
   let fixture_path = path.join(__dirname, "fixtures");
   before("Change WD to fixture dir", function() {
-    process.chdir(fixture_path)
+    process.chdir(fixture_path);
   });
 
-  it("Should compile a basic stack", async function(){
-    let tmp = new dspp("manifest.yml");
+  it("Should compile a basic stack", async function() {
+    let tmp = new Dspp("manifest.yml");
     let compiled = await tmp.parse();
     let challenge = fs.readFileSync("compiled.yml", "utf-8");
 
