12fe706fa79df2d9844c42e89570c8138416045c
Francois Leurent <131.code@leurent.email>
2022-10-20 14:20:29 +0200

    Align version in check

--- test/index.js
+++ test/index.js
@@ -6,7 +6,7 @@ const path = require('path');
 const expect = require('expect.js');
 
 const Dspp   = require('../');
-
+const {version} = require('../package.json');
 
 describe("Initial dspp check", function() {
   let fixture_path = path.join(__dirname, "fixtures");
@@ -18,6 +18,7 @@ describe("Initial dspp check", function() {
     let tmp = new Dspp("manifest.yml");
     let compiled = await tmp.parse();
     let challenge = fs.readFileSync("compiled.yml", "utf-8");
+    challenge = challenge.replace(/dspp v[0-9.]+/, `dspp v${version}`);
 
     expect(compiled).to.eql(challenge);
   });
