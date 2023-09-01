aff8ed0c11fba39938918d00adff17554340385b
Francois Leurent <131.code@leurent.email>
2023-09-01 15:19:58 +0200

    Insert floating $ with a ZWSP

--- stack.js
+++ stack.js
@@ -209,7 +209,8 @@ class dspp {
           config_map[config_name].push(line);
           out.configs[cas_name] = {name : config.name, file : cas_path};
           if(trace)
-            out.configs[cas_name]['x-trace'] = walk(trace, v =>  v.replace(/\$/g, '﹩'));
+            out.configs[cas_name]['x-trace'] = walk(trace, v =>  v.replace(/\$(?![a-z{])/gi, '$\u200B')); //insert zero width white space
+
         }
         delete out.configs[config_name];
       }
