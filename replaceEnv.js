56b4975b80ad795bc2d0a53f50c571a6e8e03444
Francois Leurent <131.code@leurent.email>
2023-09-21 23:05:35 +0200

    With replaceEnv suite test

--- replaceEnv.js
+++ replaceEnv.js
@@ -5,16 +5,23 @@ const jqdive     = require('nyks/object/jqdive');
 
 
 const replaceEnv = function(str, dict) {
-  let mask = /(?:\$\$([a-z0-9._-]+))|(?:\$\$\{([^}]+)\})/i, match;
-  if((match = mask.exec(str))) {
+  let mask = /(?:\$\$([a-z0-9._-]+))|(?:\$\$\{([^}]+)\})/ig, match;
+
+  let touched = false;
+  while((match = mask.exec(str))) {
     const key = match[1] || match[2];
     let v = jqdive(dict, key);
-    if(v !== undefined) {
-      if(typeof v == "object")
-        return v;
-      return replaceEnv(str.replace(match[0], v), dict);
-    }
+    if(v === undefined)
+      continue;
+    if(typeof v == "object")
+      return v;
+    touched = true;
+
+    str = str.replace(match[0], v);
   }
+
+  if(touched)
+    return replaceEnv(str, dict);
   return str;
 };
 
---
+++ test/replaceEnv.js
@@ -0,0 +1,34 @@
+"use strict";
+
+const expect = require('expect.js');
+
+const replaceEnv = require('../replaceEnv');
+
+describe("Testing replaceEnv", function() {
+
+  const body = "Hi $$name, $${env['color']} is great";
+
+
+  it("should do basic interpolation", async function() {
+    let env = { color : 'blue'};
+
+    let test = replaceEnv(body, {name : 'Joe', env});
+    expect(test).to.be("Hi Joe, blue is great");
+  });
+
+  it("should do incomplete interpolation", async function() {
+    let env = { color : 'blue'};
+
+    let test = replaceEnv(body, {env});
+    expect(test).to.be("Hi $$name, blue is great");
+  });
+
+  it("should full object remap", async function() {
+    let env = { color : 'blue'};
+    let test = replaceEnv("noize $$env noize", {env});
+    expect(test).to.eql(env);
+  });
+
+
+
+});
